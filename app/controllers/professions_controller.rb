d7ad5d7cbae48e2cfb96759662ccecd6acd02273
BlackE- <elizabeth.ramescamilla@gmail.com>
2014-08-23 13:10:52 -0500

    add form_for

--- app/controllers/professions_controller.rb
+++ app/controllers/professions_controller.rb
@@ -7,4 +7,23 @@ class ProfessionsController < ApplicationController
 	def show
 		@profession = Profession.find(params[:id])
 	end
+
+	def new
+		@profession = Profession.new
+	end
+
+	def create
+		@profession = Profession.new(profession_params)
+		if @profession.save
+			redirect_to @profession
+		else
+			render "new"
+		end
+	end
+
+	private 
+	def profession_params
+		params.require(:profession).permit(:name)
+	end
+
 end
